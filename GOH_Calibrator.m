9adff878094c8484c338559be4029f158ca875f5
Soheil Solhjoo <s.solhjoo@rug.nl>
2023-03-27 18:12:19 +0200

    this is a runner

--- GOH_Calibrator.m
+++ GOH_Calibrator.m
@@ -21,14 +21,14 @@ sx_22  = table2array(s_x(:,["Lambda22","Sigma22MPa"]));
 sy_11  = table2array(s_y(:,["Lambda11","Sigma11MPa"]));
 sy_22  = table2array(s_y(:,["Lambda22","Sigma22MPa"]));
 
-% data_corr = correct_data([sb_11,sb_22,sx_11,sx_22,sy_11,sy_22]);
-% 
-% sb_11 = data_corr(:,1:2);
-% sb_22 = data_corr(:,3:4);
-% sx_11 = data_corr(:,5:6);
-% sx_22 = data_corr(:,7:8);
-% sy_11 = data_corr(:,9:10);
-% sy_22 = data_corr(:,11:12);
+data_corr = correct_data([sb_11,sb_22,sx_11,sx_22,sy_11,sy_22]);
+
+sb_11 = data_corr(:,1:2);
+sb_22 = data_corr(:,3:4);
+sx_11 = data_corr(:,5:6);
+sx_22 = data_corr(:,7:8);
+sy_11 = data_corr(:,9:10);
+sy_22 = data_corr(:,11:12);
 
 sb_lam    = [sb_11(:,1) sb_22(:,1)];
 sx_lam    = [sx_11(:,1) sx_22(:,1)];
@@ -47,16 +47,22 @@ dirC = 1;    % calibrate direction: 0 (False) or 1 (True)
 [optC_GS, fval, W_func]  = W_calibrator([ss_11 ss_22], c0, CF, dirC);
 
 %%
-% file_name = "optC\CF1_dirC1_dir1.mat";
-% save(file_name, "optC_GS","fval", '-mat');
-% clear
+file_name = "optC\CF1_dirC1_dir2.mat";
+save(file_name, "optC_GS","fval", '-mat');
 % load(file_name)
 %% Visualize the results
-[sb_GOH_GS, invariants, d_energy] = W_GOH_stress(sb_lam, optC_GS);
+sb_temp = [ linspace(sb_lam(1,1),sb_lam(end,1))', ...
+            linspace(sb_lam(1,2),sb_lam(end,2))'];
+sx_temp = [ linspace(sx_lam(1,1),sx_lam(end,1))', ...
+            linspace(sx_lam(1,2),sx_lam(end,2))'];
+sy_temp = [ linspace(sy_lam(1,1),sy_lam(end,1))', ...
+            linspace(sy_lam(1,2),sy_lam(end,2))'];
+
+[sb_GOH_GS, invariants, d_energy] = W_GOH_stress(sb_temp, optC_GS);
 ss_plot([sb_11 sb_22],"Equibiaxial")
 hold on;
-plot(sb_lam(:,1),sb_GOH_GS(:,1),'b-')
-plot(sb_lam(:,2),sb_GOH_GS(:,2),'r-')
+plot(sb_temp(:,1),sb_GOH_GS(:,1),'b-')
+plot(sb_temp(:,2),sb_GOH_GS(:,2),'r-')
 hold off
 
 % sx_GOH_GS = W_GOH_stress(sx_lam, optC_GS);
@@ -73,19 +79,30 @@ hold off
 % plot(sy_lam(:,2),sy_GOH_GS(:,2),'r-')
 % hold off
 %%
-cal_e = zeros(size(sb_lam,1),1);
+close all
+cal_e = zeros(size(sb_temp,1),1);
 for i = 1:2
-    cal_e(:,i) = cumtrapz(sb_lam(:,i), sb_GOH_GS(:,i));
+    cal_e(:,1) = cal_e(:,1) + cumtrapz(log(sb_temp(:,i)), sb_GOH_GS(:,i));
 end
-%%
+
 energy = GOH_energy(optC_GS,invariants);
 % e_temp = energy;
 e_temp = energy - energy(1);
 % close all
 figure 
 hold on
-plot(sb_lam(:,1), e_temp,'b-')
-plot(sb_lam(:,2), e_temp,'r-')
+plot(sb_temp(:,1), e_temp,'b-')
+% plot(sb_temp(:,2), e_temp,'r-')
 
-plot(sb_lam(:,1), cal_e(:,1),'b*')
-plot(sb_lam(:,2), cal_e(:,2),'r*')
+plot(sb_temp(:,1), cal_e,'r--')
+% plot(sb_temp(:,2), cal_e,'r--')
+
+% plot(sb_temp(:,1), cal_e(:,1),'b*')
+% plot(sb_temp(:,2), cal_e(:,2),'r*')
+%% stress = d_energy / d_strain
+% sig_der = gradient(energy)./gradient(sb_temp).*invariants(:,3).^2;
+% ss_plot([sb_11 sb_22],"Equibiaxial")
+% hold on;
+% plot(sb_temp(:,1),sig_der(:,1),'b-')
+% plot(sb_temp(:,2),sig_der(:,2),'r-')
+% hold off
