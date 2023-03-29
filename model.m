1e97c032951d0b04b73de5db433fb7d7f436a159
Soheil Solhjoo <s.solhjoo@rug.nl>
2023-03-29 14:09:51 +0200

    Update model.m

--- model.m
+++ model.m
@@ -1,21 +1,22 @@
-function U = model(parameters,X)
+function W = model(parameters,I0)
 
+% I0 = [I1; I41];
 numLayers = numel(fieldnames(parameters))/2;
 
 % First fully connect operation.
 weights = parameters.fc1_Weights;
 bias = parameters.fc1_Bias;
-U = fullyconnect(X,weights,bias);
+W = fullyconnect(I0,weights,bias);
 
 % tanh and fully connect operations for remaining layers.
 for i=2:numLayers
     name = "fc" + i;
 
-    U = tanh(U);
+    W = tanh(W);
 
     weights = parameters.(name + "_Weights");
     bias = parameters.(name + "_Bias");
-    U = fullyconnect(U, weights, bias);
+    W = fullyconnect(W, weights, bias);
 end
 
 end
