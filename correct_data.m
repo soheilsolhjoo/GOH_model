1d200f83e445de71a9542697bd5906b8903fcbaf
Soheil Solhjoo <s.solhjoo@rug.nl>
2023-03-31 17:12:55 +0200

    convert MPa to KPa

--- correct_data.m
+++ correct_data.m
@@ -3,6 +3,13 @@ function data = correct_data(data)
 % The code assumes raw_data: data_1 | data_2 | ...
 data(1,:) = [];
 for i = 1 : size(data,2)
-    data(:,i) = data(:,i) - min(data(:,i)) + mod(i,2);
+    corr = mod(i,2);
+    MPa2KPa = (1-corr) * 1000 + corr;
+    if corr
+        data(:,i:i+1) = sortrows(data(:,i:i+1),1);
+    end
+    % data(:,i) = (data(:,i) - min(data(:,i)) + corr) * MPa2KPa;
+    data(:,i) = (data(:,i) - data(1,i) + corr) * MPa2KPa;
+
 end
 end
