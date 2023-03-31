333a9bf13c3d8b4a7588eb6a535496a2ea033a33
Soheil Solhjoo <s.solhjoo@rug.nl>
2023-03-31 17:28:09 +0200

    corrected W_aniso

--- calc_i2p.m
+++ calc_i2p.m
@@ -11,6 +11,6 @@ W_iso   = C1 * (inv(:,1)-3);
 E       = kap * (inv(:,1)-3) + (1-3*kap) * (inv(:,2:2+n_I4-1)-1);
 % E       = kap * inv(:,1) + (1-3*kap) * inv(:,2:2+n_I4-1) - 1;
 E       = (abs(E) + E)./2;
-W_aniso = k1 / (2 * k2) * sum(exp((k2 * E.^2)-1),2);
+W_aniso = k1 / (2 * k2) * (sum(exp((k2 * E.^2))-1,2));
 W       = W_iso + W_aniso;
 end
