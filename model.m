7d660dc64ac6ec57517f0cf27d8bf56de55c6767
Soheil Solhjoo <s.solhjoo@rug.nl>
2023-05-02 14:46:43 +0200

    Update model.m
    it takes normal variables with no call of the dlnetwork platform.

--- model.m
+++ model.m
@@ -1,12 +1,12 @@
 function W = model(parameters,I0)
 
-% I0 = [I1; I41];
+% I0 = [I1; I41; I42];
 numLayers = numel(fieldnames(parameters))/2;
 
 % First fully connect operation.
 weights = parameters.fc1_Weights;
 bias = parameters.fc1_Bias;
-W = fullyconnect(I0,weights,bias);
+W = fully_connect(I0,weights,bias);
 
 % tanh and fully connect operations for remaining layers.
 for i=2:numLayers
@@ -16,7 +16,11 @@ for i=2:numLayers
 
     weights = parameters.(name + "_Weights");
     bias = parameters.(name + "_Bias");
-    W = fullyconnect(W, weights, bias);
+    W = fully_connect(W, weights, bias);
 end
 
+end
+
+function FC = fully_connect(X,W,b)
+FC = W*X + b;
 end
