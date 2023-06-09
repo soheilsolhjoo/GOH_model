f9988e6844ca2d298d966d619bc06837f17a4fd9
Soheil Solhjoo <s.solhjoo@rug.nl>
2023-06-09 16:13:04 +0200

    Update consts.py

--- Python/consts.py
+++ Python/consts.py
@@ -8,3 +8,9 @@ alpha = [0,90]  # fiber directions
 
 # constants of the Gasser-Ogden-Holzapfel (GOH) model
 const = [0.1,1,1,1/6] # [mu, k1, k2, kappa]
+
+# define column names for later data collection
+I_col = ["I1", "I41", "I42"]
+W_col = ["Energy_exp"]
+L_col = ['Lambda11(-)', 'Lambda22(-)', 'Lambda33(-)']
+S_col = ['Sigma11(KPa)', 'Sigma22(KPa)']
